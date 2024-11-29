da3622cb5fbb97609c77dbf1bb1a6e4e06bcf607
pmoleri <pmoleri@gmail.com>
2024-11-29 14:02:59 -0300

    Test2 updates

---
+++ src/app/view1/view1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view1',
+  imports: [],
+  templateUrl: './view1.component.html',
+  styleUrls: ['./view1.component.scss']
+})
+export class View1Component {
+}
--- src/app/view1/view1.test.tsx
+++
@@ -1,9 +0,0 @@
-import { expect, test } from 'vitest';
-import { render } from '@testing-library/react';
-import View1 from './view1';
-import 'element-internals-polyfill';
-
-test('renders View1 component', () => {
-  const wrapper = render(<View1 />);
-  expect(wrapper).toBeTruthy();
-});
